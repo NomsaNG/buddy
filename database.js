15909a9fb4b9640d950a54e07ce1d5bb65564988
Nomsa Goba <nomsagoba@Johan.local>
2025-05-13 16:55:52 +0200

    base working

---
+++ database.js
@@ -0,0 +1,11 @@
+import Dexie from './node_modules/dexie/dist/dexie.mjs';
+
+export const db = new Dexie("MyBuddyAppDB");
+
+// Define your database schema
+db.version(1).stores({
+  users: '++id, name, email, income, password, expenses, transactions',// Auto-incrementing ID
+  finances: '++id, userId, category, amount, classification, date',  // Example: Financial Records
+});
+
+export default db;
